bf60b14801d6a7ad372ea9678dd5345b27d814c4
Mohammad-Eusuf <71366614+Mohammad-Eusuf@users.noreply.github.com>
2020-09-21 18:26:00 +0530

    First version of Placecollection
    Basic methods

---
+++ placecollection.py
@@ -0,0 +1,15 @@
+
+class PlaceCollection:
+  def __init__(self):
+    self.places = []
+
+  def add_place(self,placeObject):
+    self.places.append(placeObject)
+    
+  def load_places(self):
+    print( "")
+  def get_visited_places(self):
+    print( "")
+    
+  def sort_values(self):
+    print( "")
