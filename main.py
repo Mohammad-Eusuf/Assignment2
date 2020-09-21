44ee0623db24509036842b9a899cac5f1287e796
Mohammad-Eusuf <71366614+Mohammad-Eusuf@users.noreply.github.com>
2020-09-21 19:06:36 +0530

    Final Version
    Final Version

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import place
-import placeollection
+import placecollection
 
 def main():
-	print("\nMenu")
-    print("\n1. Load CSV file")    
-    print("\n2. Save to CSV file")    
-    print("\n3. Add Places")    
-    print("\n4. Get number of unvisited places")    
-    print("\n5. Sort the places by Priority")
-    choice=int(input("\nEnter between 1-5"))
-    place_collection=PlaceCollection()
-    
-    if (choice==3):
-        name=input("\nEnter the name of the place")        
-        country=input("\nEnter the country")        
-        priority=input("\nEnter the priority of place")        
-        status=input("\nEnter the place visited status (Y or N)")
-        p1 = place(name,country,priority,status)
-        place_collection.add_place(p1)
-        print("\nPlace Added Successfully")
+    choice=1
+    while (choice>0 and choice<6):
+        print("\n**********Menu*****************\n")
+        print("\n1. Load CSV file")    
+        print("\n2. Save to CSV file")    
+        print("\n3. Add Places")    
+        print("\n4. Get number of unvisited places")    
+        print("\n5. Sort the places by Priority")
+        choice=int(input("\nEnter between 1-5 : "))
+        place_collection=placecollection.PlaceCollection()
+
+        if (choice==1):
+            place_collection.load_places()
+            print("File is uploaded successfully")
+            
         
-    if (choice==4):
-        print("\nThe Number of unvisited places are " + str(place_collection.get_visited_places))
+        if (choice==2):
+            place_collection.save_places()
+            print("The file downloaded successfully")
+        if (choice==3):
+            name=input("\nEnter the name of the place : ")        
+            country=input("\nEnter the country : ")        
+            priority=input("\nEnter the priority of place : ")        
+            status=input("\nEnter the place visited status (Y or N) : ")
+            p1 = place.place(name,country,priority,status)
+            place_collection.add_place(p1)
+            print("\nPlace Added Successfully")
+        
+        if (choice==4):
+            print("\nThe Number of unvisited places are " + str(place_collection.get_visited_places()))
+
+        if (choice==5):
+            print("\nBelow are the places : ")
+            place_collection.sort_values()
+        
+        
+main()
--- placecollection.py
+++ placecollection.py
@@ -8,10 +8,20 @@ class PlaceCollection:
     self.places.append(placeObject)
     
   def load_places(self):
-    print( "")
+    filename=input("Please provide the file to load : ")
+    fh=open(filename,"r")
+    for i in fh:
+        fields=i.split(",")
+        p1=place(fields[0],fields[1],fields[3],fields[2])
+        self.places.append(p1)
+        
 
   def save_places(self):
-    print( "")
+    fh=open("TravelDetails.csv","w")
+    for pl in self.places:
+        fh.write(pl.name + "," + pl.country + "," + pl.visited_status + "," + pl.priority() + "\n")
+
+    fh.close()    
     
   def get_visited_places(self):
     count=0
@@ -21,4 +31,6 @@ class PlaceCollection:
     return count
     
   def sort_values(self):
-    print( "")
+    for pl in self.places:
+        print("\nPlace : "+pl.name + " country : " + pl.country + " Visited Status : " + pl.visited_status + " Priority : " + pl.is_important())
+        print("\n")
