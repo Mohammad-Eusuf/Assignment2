706f23ee5fc880d198ad7629a3ac64e6a10c1d2e
Mohammad-Eusuf <71366614+Mohammad-Eusuf@users.noreply.github.com>
2020-09-21 18:17:44 +0530

    Added Method to check Importance of place
    Added Method to check Importance of place

--- place.py
+++ place.py
@@ -6,11 +6,17 @@ class place:
     self.priority = priority
     self.visited_status = visited_status
     
-  def __str__(self, name, age):
-    print("")
+  def __str__(self):
+    print("\nPlace : "+self.name + " country : " + self.country + " Visited Status : " + self.visited_status + " Priority : " + self.is_important())
     
   def mark_visited(self):
     self.visited_status='Y'
     
   def mark_unvisited(self):
     self.visited_status='N'
+    
+  def is_important(self):
+    if self.priority<=2:
+        return "Important"
+    else:
+        return "Not Important"
